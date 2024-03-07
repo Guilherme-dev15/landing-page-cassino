d649b43609690bac32265b3a1114f914108fc69f
guilherme dev <guilherme.macedo1598@gmail.com>
2024-03-07 16:53:15 -0300

    Update Accodion transition

--- assets/js/main-faq.js
+++ assets/js/main-faq.js
@@ -6,26 +6,24 @@ fetchFaqData()
         const itemColumn_A = document.getElementById('column-items_A');
         const itemColumn_B = document.getElementById('column-items_B');
 
-
         // Iterate through each FAQ item in the JSON data
         data.faq.forEach((faq, index) => {
             const itemContent = document.createElement('div');
             itemContent.classList.add('accordion-item');
             itemContent.innerHTML = `
-                <h2 class="accordion-header">
-                    <button class="accordion-button btn-open-faq">
-                        ${faq.title}
+          <h2 class="accordion-header">
+              <button class="accordion-button btn-open-faq">
+                  ${faq.title}
 
-                        <i class="fa fa-plus" aria-hidden="true"></i>
-                    </button>
-                </h2>
-                <div class="accordion-content display-none" id="accordion-content-faq">
-                    <p class="faq-answer">
-                        ${faq.content}
-                    </p>
-                </div>
-            `;
-
+                  <i class="fa fa-plus" aria-hidden="true"></i>
+              </button>
+          </h2>
+          <div class="accordion-content">
+              <p class="faq-answer">
+                  ${faq.content}
+              </p>
+          </div>
+      `;
 
             // Add the FAQ item to the main container
             if (index < data.faq.length / 2) {
@@ -34,28 +32,36 @@ fetchFaqData()
                 itemColumn_B.appendChild(itemContent);
             }
 
-        });
+            // Set the accordion content to display: none
+            itemContent.querySelector('.accordion-content').classList.add('accordion-content-hidden');
 
-
-
-        // Select all the buttons and accordion containers elements after they have been added to the DOM
-        const btnOpen = document.getElementsByClassName('btn-open-faq');
-
-        // Add a click event listener to each button to toggle the accordion container visibility
-        for (let i = 0; i < btnOpen.length; i++) {
-            btnOpen[i].addEventListener('click', function () {
+            // Add a click event listener to each button to toggle the accordion container visibility
+            itemContent.querySelector('.accordion-button').addEventListener('click', function () {
                 const accordion = this.parentElement.nextElementSibling;
-                if (accordion.classList.contains('display-none')) {
-                    accordion.classList.remove('display-none');
+
+                // Toggle the accordion content visibility
+                accordion.classList.toggle('accordion-content-hidden');
+                accordion.classList.toggle('open');
+
+                // Remove the transition property to prevent it from being applied twice
+                accordion.style.transition = '';
+
+                // Adjust the height of the accordion content
+                if (accordion.classList.contains('accordion-content-hidden')) {
+                    accordion.style.height = '0px';
                 } else {
-                    accordion.classList.add('display-none');
+                    accordion.style.height = accordion.scrollHeight + 'px';
                 }
+
+                // Add the transition effect to the accordion content
+                accordion.style.transition = 'height 0.3s ease-out';
+
+                // Toggle the active class on the accordion button
+                this.classList.toggle('active');
             });
-        }
+        });
     })
     .catch(error => {
         console.error('Erro geral:', error.message);
         // Handle the error appropriately
     });
-
-
